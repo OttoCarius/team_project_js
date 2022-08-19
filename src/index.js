54f1fefd3d1020b213497963b42a61db0a7383c2
OttoCarius <ottocarius1993@gmail.com>
2022-08-19 18:18:19 +0300

    finish header

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-
 import { refs } from './js/refs';
 import { renderMarkup, markupHeroLetters, onLetterClick } from './js/hero';
 import './js/createMarkup.js';
 import './js/cardByLetter.js';
+import './js/header';
 
 refs.heroList.addEventListener('click', onLetterClick);
 
 renderMarkup(refs.heroList, markupHeroLetters);
-
-
