0a7a3c668d83e38e87919c6e5ac08caf2511917a
Yana-Palam <tana.palam@gmail.com>
2022-08-25 10:48:15 +0300

    fix

--- src/js/pageFavCocktails.js
+++ src/js/pageFavCocktails.js
@@ -1,23 +1,16 @@
 import { refs } from './refs';
-import {
-  createRandomMarkup,
-  filterQuantityItems,
-  renderMarkup,
-} from './markup';
-// import { storageCocktailByLetter } from './storageCocktails';
+import { createRandomMarkup, renderMarkup } from './markup';
 import data from './object';
 import { fetchCocktail } from '..';
 import { FAVOURITE_KEY } from './addToFavourite';
 
 export async function onPageFavCocktails() {
   refs.heroSection.style.display = 'none';
-  //   console.log(refs.cocktailTitle);
   refs.cocktailTitle.textContent = 'Favorite cocktails';
   refs.cocktailTitle.parentNode.style.marginTop = '60px';
   refs.listCocktail.innerHTML = '';
 
   const parsedArray = JSON.parse(localStorage.getItem(FAVOURITE_KEY));
-
   if (!parsedArray) {
     return;
   }
@@ -25,14 +18,11 @@ export async function onPageFavCocktails() {
     return fetchCocktail.getResultsById(id);
   });
   const res = await Promise.all(array);
-  //   console.log('res', res);
 
-  //   console.log('data', data);
   const markup = createRandomMarkup(res, data);
-  //   console.log(markup);
-
-  const drinks = filterQuantityItems(markup);
-  renderMarkup(refs.listCocktail, drinks);
-
-  //   storageCocktailByLetter();
+  renderMarkup(refs.listCocktail, markup);
+  const arrBtnAddTo = document.querySelectorAll('.btn-add-to-favorite');
+  arrBtnAddTo.forEach(element => {
+    element.textContent = 'Remove';
+  });
 }
